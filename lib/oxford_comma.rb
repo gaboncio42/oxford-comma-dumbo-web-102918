0873684a9c2145e6f1b56dae17e6616518438578
Gabriel Seijo <gabrielseijo.m@gmail.com>
2018-10-15 19:00:29 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -8,7 +8,7 @@ def oxford_comma(array)
   else array.length >= 3
     array.insert(-2, "and")
     array.pop
-    more_than_two.conca
+    more_than_two.concat()
     more_than_two = array.join(", ")
   end
 end
