fc5c0150acbbb407a09928b7d00b2258adf94445
Gabriel Seijo <gabrielseijo.m@gmail.com>
2018-10-15 18:07:06 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,8 +2,9 @@ def oxford_comma(array)
   if array.length == 1
     one_element = array.join
     return one_element
-  else array.length == 2
+  elsif array.length == 2
     two_elements = array.join(" and ")
     return two_elements
+  else array length >= 3
   end
 end
